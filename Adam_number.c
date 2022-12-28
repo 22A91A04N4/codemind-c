ee7cd28028af4723236cfe75ab28d765b8ef3bee
22A91A04N4 <120077275+22A91A04N4@users.noreply.github.com>
2022-12-28 05:30:51 +0530

    #include<stdio.h> int main() {     int n,q,s=0,r,sn1,sn2,rsn2;     scanf("%d",&n);     sn1=n*n;     q=n;     while(q!=0)     {         r=q%10;         s=s*10+r;         q=q/10;     }     sn2=s*s;     q=sn2;     s=0;     while(q!=0)     {         r=q%10;         s=s*10+r;         q=q/10;     }     rsn2=s;     if(sn1==rsn2)     {         printf("True");     }     else     {         printf("False");     }
    }

---
+++ Adam_number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,q,s=0,r,sn1,sn2,rsn2;
+    scanf("%d",&n);
+    sn1=n*n;
+    q=n;
+    while(q!=0)
+    {
+        r=q%10;
+        s=s*10+r;
+        q=q/10;
+    }
+    sn2=s*s;
+    q=sn2;
+    s=0;
+    while(q!=0)
+    {
+        r=q%10;
+        s=s*10+r;
+        q=q/10;
+    }
+    rsn2=s;
+    if(sn1==rsn2)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    
+}
