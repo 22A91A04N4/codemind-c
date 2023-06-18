c05837a41caa5d92f5df7bbcd1e7b10918b9d770
22A91A04N4 <120077275+22A91A04N4@users.noreply.github.com>
2023-06-18 14:03:06 +0530

    #include<stdio.h> int palindrome(int n) {     int s=0,q,r,rem;     scanf("%d",&n);     q=n;    while(q!=0)    {        r=q%10;        s=s*10+r;        q=q/10;    }    if(s==n)    {        return 1;    }    else    {        return 0;    } } int main() {     int  n,i=1,count=0,k;     scanf("%d",&n);     while(n>0)     {         if(palindrome(n+i)&&palindrome(n-i))         {             printf("%d %d",n-i,n+i);             break;         }         else if(palindrome(n+i))         {             printf("%d",n+i);             break;         }          else if(palindrome(n-i))         {             printf("%d",n-i);             break;         }         i++;     } }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -4,24 +4,24 @@ int palindrome(int n)
     int s=0,q,r,rem;
     scanf("%d",&n);
     q=n;
-    while(q!=0)
-    {
-        r=q%10;
-        s=s*10+r;
-        q=q/10;
-    }
-    if(s==n)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+   while(q!=0)
+   {
+       r=q%10;
+       s=s*10+r;
+       q=q/10;
+   }
+   if(s==n)
+   {
+       return 1;
+   }
+   else
+   {
+       return 0;
+   }
 }
 int main()
 {
-    int n,i=1,count=0,k;
+    int  n,i=1,count=0,k;
     scanf("%d",&n);
     while(n>0)
     {
@@ -35,7 +35,7 @@ int main()
             printf("%d",n+i);
             break;
         }
-        else if(palindrome(n-i))
+         else if(palindrome(n-i))
         {
             printf("%d",n-i);
             break;
