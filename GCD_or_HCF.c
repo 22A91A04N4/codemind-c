ea9a5e29556ae4a38043c25f98fe57ef9191afd0
22A91A04N4 <120077275+22A91A04N4@users.noreply.github.com>
2023-06-18 13:40:01 +0530

    #include<stdio.h> int main() {     int a,b,hcf,i;     scanf("%d%d",&a,&b);     for(i=1;i<=a||i<=b;i++)     {         if(a%i==0&&b%i==0)         {             hcf=i;         }     }     printf("%d",hcf); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,hcf;
-    scanf("%d %d",&a,&b);
-    for(int i=1;i<=a||i<=b;i++)
+    int a,b,hcf,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=a||i<=b;i++)
     {
         if(a%i==0&&b%i==0)
         {
